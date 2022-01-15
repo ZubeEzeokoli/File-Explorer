99d063bc9fbcff876a453df2b5685e64de58c3ce
Nzubechi Ezeokoli <62896253+ZubeEzeokoli@users.noreply.github.com>
2022-01-15 14:15:22 -0800

    Update a1.py

--- a1.py
+++ a1.py
@@ -1,12 +1,3 @@
-# a1.py
-
-# Starter code for assignment 1 in ICS 32 Programming with Software Libraries in Python
-
-# Replace the following placeholders with your information.
-
-# Nzubechi Ezeokoli
-# nezeokol@uci.edu
-# 56611321
 from pathlib import Path
 inp = input()
 def recurse(p):
