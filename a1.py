997f01f5e2b986fe055b04e8e5e95bb29c466ab2
Nzubechi Ezeokoli <62896253+ZubeEzeokoli@users.noreply.github.com>
2022-01-31 10:51:38 -0800

    Add files via upload

--- a1.py
+++ a1.py
@@ -8,77 +8,7 @@
 # nezeokol@uci.edu
 # 56611321
 from pathlib import Path
-def recurse(p):
-    """recursively prints the contents of directories or files depending on what option is chossen"""
-    if option[-1] == '-r':
-        #Prints files and directories in specified directory recursively
-        ctr = 0
-        if ctr == 0:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_file():
-                    print(obj)
-                    ctr += 1
-                else:
-                    ctr = -1
-        if ctr > 0 or ctr == -1:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_dir():
-                    print(obj)
-                    recurse(obj)
-    elif option[-1] == '-f':
-        #Prints files in specified directory recursively
-        ctr = 0
-        if ctr == 0:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_file():
-                    print(obj)
-                    ctr += 1
-                else:
-                    ctr = -1
-        if ctr > 0 or ctr == -1:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_file():
-                    if not Path(obj).exists():
-                        print(obj)
-                else:
-                    recurse(obj)
-    elif option[-1] == '-s':
-        #Prints files with the same name as the name given in input recursively
-        ctr = 0
-        if ctr == 0:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_file() and obj.name == suffix:
-                    print(obj)
-                    ctr += 1
-                else:
-                    ctr = -1
-        if ctr > 0 or ctr == -1:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_dir():
-                    recurse(obj)
-                elif obj.is_file() and obj.name == suffix:
-                    if not Path(obj).exists():
-                        print(obj)
-    elif option[-1] == '-e':
-        #Prints files with the same suffix as the suffix given in the input recursively
-        ctr = 0
-        if ctr == 0:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_file() and obj.suffix[1:] == suffix:
-                    print(obj)
-                    ctr += 1
-                else:
-                    ctr = -1
-        if ctr > 0 or ctr == -1:
-            for obj in sorted(Path(p).iterdir()):
-                if obj.is_dir():
-                    recurse(obj)
-                elif obj.is_file() and obj.suffix[1:] == suffix:
-                    if not Path(obj).exists() and obj.suffix[1:]:
-                        print(obj)
-            
-            
-        
+import m1
 
 inp = input()
 while inp[:1].upper() != 'Q':
@@ -121,87 +51,15 @@ while inp[:1].upper() != 'Q':
             else:
                 suffix += extra[-1]
             if inp[:1].upper() == 'L':
-                #Lists the contents of the user specified directory and performs output based on option and if user wants to recursively
-                if len(option) > 1 or len(option) == 1 and extra[0] == '-r':
-                    #Recursive call if option has more than 1 element in its list such as ['-r', '-f'] or if it is only -r
-                    if len(extra) == 1 and extra[0] == '-r':
-                        recurse(p)
-                    elif extra[1] == '-f' or extra[1] == '-s' or extra[1] == '-e':
-                        recurse(p)
-                    else:
-                        print('ERROR')
-                elif len(option) == 1:
-                        if option[0] == '-f':
-                            #Prints files in specified directory
-                            for obj in sorted(Path(p).iterdir()):
-                                if obj.is_file():
-                                    print(obj)
-                        if option[0] == '-s':
-                            #Prints files with the same name as the name given
-                                for obj in sorted(Path(p).iterdir()):
-                                    if obj.name == suffix:
-                                        print(obj)
-                        if option[0] == '-e':
-                            #Prints files with the same suffix as the suffix given
-                            for obj in sorted(Path(p).iterdir()):
-                                if obj.suffix[1:] == suffix:
-                                    print(obj)
-                elif len(option) == 0:
-                    #If no option is given just prints everything in one directory that was specified
-                    try:
-                        p = inp[2:]
-                        for obj in sorted(Path(p).iterdir()):
-                            if obj.is_file():
-                                print(obj)
-                        for obj in sorted(Path(p).iterdir()):
-                            if obj.is_dir():
-                                print(obj)
-                    except FileNotFoundError:
-                        print('Error')
-                else:
-                    print('ERROR')
-                    
-                inp = input()
+                m1.look(option, extra, suffix, p, inp)
             elif inp[:1].upper() == 'C':
-                #Creates a new file in the specified directory
-                if option[-1] == '-n':
-                    new_file = suffix + '.dsu'
-                    p1 = Path(p) / new_file
-                    if not p1.exists():
-                        p1.touch()
-                        print(p1)
-                else:
-                    print('ERROR')
-                inp = input()
+                m1.create(option, suffix, p)
             elif inp[:1].upper() == 'D':
-                #Deletes the file that is specified
-                p = inp[2:]
-                if p[-3:] == 'dsu':
-                    Path(p).unlink()
-                    print(p, 'DELETED')
-                    inp = input()
-                else:
-                    print('ERROR')
-                    inp = input()
+                m1.delete(inp)
             elif inp[:1].upper() == 'R':
-                #Reads the contents of a file that is specified
-                p = inp[2:]
-                if p[-3:] == 'dsu':
-                    f = Path(p).open()
-                    r = f.read()
-                    if r == '':
-                        print('EMPTY')
-                        inp = input()
-                    else:
-                        print(r, end='')
-                        inp = input()
-                    f.close()
-                else:
-                    print('ERROR')
-                    inp = input()
+                m1.read(inp)
         else:
             print('ERROR')
-            inp = input()
     else:
         print('ERROR')
-        inp = input()
+    inp = input()
